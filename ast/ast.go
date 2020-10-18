5ba5997c028266a65d175d53e59fed9066c5760f
Xingjian Xie <xiexingjian2003@gmail.com>
2020-10-18 12:22:51 +0800

    Fix Repl String

--- ast/ast.go
+++ ast/ast.go
@@ -271,9 +271,9 @@ func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
 func (ie *IfExpression) String() string {
 	var out bytes.Buffer
 
-	out.WriteString("if ")
+	out.WriteString("if (")
 	out.WriteString(ie.Condition.String())
-	out.WriteString(" ")
+	out.WriteString(") ")
 	out.WriteString(ie.Consequence.String())
 
 	if ie.Alternative != nil {
@@ -295,9 +295,9 @@ func (le *LoopExpression) TokenLiteral() string { return le.Token.Literal }
 func (le *LoopExpression) String() string {
 	var out bytes.Buffer
 
-	out.WriteString("if ")
+	out.WriteString("loop (")
 	out.WriteString(le.Condition.String())
-	out.WriteString(" ")
+	out.WriteString(") ")
 	out.WriteString(le.Body.String())
 
 	return out.String()
@@ -317,9 +317,9 @@ func (li *LoopInExpression) String() string {
 
 	out.WriteString("loop ")
 	out.WriteString(li.Name.Value)
-	out.WriteString(" in ")
+	out.WriteString(" in (")
 	out.WriteString(li.Range.String())
-	out.WriteString(" ")
+	out.WriteString(") ")
 	out.WriteString(li.Body.String())
 
 	return out.String()
